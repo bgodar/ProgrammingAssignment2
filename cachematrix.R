0f91dcf0838a55705d6253bd1b6eebbea2ed13ed
bgodar <bryna.godar@gmail.com>
2017-01-01 13:28:35 -0600

    My solution to programming assignment 2 on matrix inversion.

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a matrix object and allow you to
+## cache its inverse
 
 ## makeCacheMatrix creates a special "matrix" object that can cache
 ## its inverse
